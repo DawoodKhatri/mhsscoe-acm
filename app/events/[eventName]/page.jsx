18cb39b75b47f43fbcb22979941e9d8034737493
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-09-21 23:08:39 +0530

    fixed mdx error and added some events

--- app/events/[eventName]/page.jsx
+++ app/events/[eventName]/page.jsx
@@ -1,38 +1,31 @@
-import EventNotFound from "@/components/event/notFound";
-// import { EVENT_DATA } from "@/constants/data";
-
-// mdx imports here :)
 import { allEvents } from "contentlayer/generated";
-import { useMDXComponent } from "next-contentlayer/hooks";
-import MDXImage from "@/components/event/mdxImage";
+import EventDetails from "@/components/event/eventDetails";
+import EventNotFound from "@/components/event/notFound";
 
 const EventPage = ({ params: { eventName } }) => {
   const event = allEvents.find(
-    (event) => event._raw.flattenedPath === eventName
+    (event) => event._raw.flattenedPath === decodeURI(eventName)
   );
 
-  // if (!event) return <EventNotFound />;
-  const MDXContent = useMDXComponent(event.body.code);
+  if (!event) return <EventNotFound />;
 
-  const contents = {
-    MDXImage,
-  };
+  return <EventDetails title={event.title} bodyCode={event.body.code} />;
 
-  return (
-    <article className="mx-auto max-w-xl py-8">
-      <div className="mb-8 text-center">
-        <time dateTime={event.date} className="mb-1 text-xs text-gray-600">
-          {/* indian date time format */}
+  // return (
+  //   <article className="mx-auto max-w-xl py-8">
+  //     <div className="mb-8 text-center">
+  //       <time dateTime={event.date} className="mb-1 text-xs text-gray-600">
+  //         {/* indian date time format */}
 
-          {new Intl.DateTimeFormat("en-IN").format(new Date(event.date))}
-        </time>
-        <h1 className="text-3xl font-bold">{event.title}</h1>
-        <div className="prose text-left">
-          <MDXContent components={contents} />
-        </div>
-      </div>
-    </article>
-  );
+  //         {new Intl.DateTimeFormat("en-IN").format(new Date(event.date))}
+  //       </time>
+  //       <h1 className="text-3xl font-bold">{event.title}</h1>
+  //       <div className="prose text-left">
+  //         <MDXContent components={contents} />
+  //       </div>
+  //     </div>
+  //   </article>
+  // );
 };
 
 export default EventPage;
--- components/event/eventDetails.jsx
+++ components/event/eventDetails.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import Glassmorphism from "../common/glassmorphism";
 import { Col, Row } from "antd";
+import { useMDXComponent } from "next-contentlayer/hooks";
 
-const EventDetails = ({ details }) => {
-  const {
-    start_date,
-    end_date,
-    registration_end_date,
-    entry_fees_members,
-    entry_fees_others,
-  } = details.info;
+const EventDetails = ({ title, bodyCode }) => {
+  let contents = {
+    img: ({ src, alt }) => (
+      <div className="relative overflow-hidden rounded-lg">
+        <img className="opacity-80 w-full m-0" src={src} />
+        {alt && (
+          <div
+            className={`absolute top-0 w-full h-full flex justify-center items-end bg-gradient-to-b from-transparent to-primary-dark  opacity-0 hover:opacity-100 transition-all duration-300`}
+          >
+            <p className="text-xl m-5 italic text-center text-white">{alt}</p>
+          </div>
+        )}
+      </div>
+    ),
+  };
+
+  const MDXContent = useMDXComponent(bodyCode);
+
   return (
-    <>
+    <article>
       <Glassmorphism className="mb-8 p-8 pt-2 md:m-5">
         <h2
-          className={`text-5xl pt-3 md:text-7xl md:pt-0 font-bold italic text-center text-primary-dark`}
+          className={`text-4xl pt-3 sm:text-5xl md:text-6xl lg:text-7xl md:pt-0 font-bold italic text-center text-primary-dark`}
         >
-          {details?.title}
+          {title}
         </h2>
       </Glassmorphism>
 
@@ -29,69 +40,112 @@ const EventDetails = ({ details }) => {
           md={{ span: 20 }}
           lg={{ span: 16, order: 0 }}
         >
-          {details?.contents.map(
-            ({ type, image, description, text }, index) => (
-              <div key={`event_page_content_${index}`}>
-                {type === "image" && (
-                  <Glassmorphism className="relative overflow-hidden">
-                    <img className="opacity-80 w-full" src={image} />
-                    <div
-                      className={`absolute top-0 w-full h-full flex justify-center items-end bg-gradient-to-b from-transparent to-primary-dark  opacity-0 hover:opacity-100 transition-all duration-300`}
-                    >
-                      <p className="text-xl m-5 italic text-center text-white">
-                        {description}
-                      </p>
-                    </div>
-                  </Glassmorphism>
-                )}
-
-                {type === "text" && (
-                  <Glassmorphism className="my-5">
-                    <p className="text-xl m-5">{text}</p>
-                  </Glassmorphism>
-                )}
-              </div>
-            )
-          )}
+          <Glassmorphism className="prose max-w-full w-full p-8 my-5">
+            <MDXContent components={contents} />
+          </Glassmorphism>
         </Col>
-        <Col
+        {/* <Col
           className="lg:pl-8 pb-5 lg:pb-0"
           span={24}
           order={0}
           sm={{ span: 22 }}
           md={{ span: 20 }}
           lg={{ span: 8, order: 1 }}
-        >
-          <Glassmorphism className={`p-5 text-lg text-primary-dark`}>
-            <p className="my-2">
-              <span className={`font-semibold`}>Event Starts: </span>
-              <i>{start_date.toDateString()}</i>
-            </p>
-
-            <p className="my-2">
-              <span className="font-semibold">Event Ends: </span>
-              {end_date.toDateString()}
-            </p>
-
-            <p className="my-2">
-              <span className="font-semibold">Registration Ends: </span>
-              {registration_end_date.toDateString()}
-            </p>
-
-            <p className="my-2">
-              <span className="font-semibold">Entry Fees for Members: </span>
-              {entry_fees_members ? "₹" + entry_fees_members : "Free"}
-            </p>
-
-            <p className="my-2">
-              <span className="font-semibold">Entry Fees for Others: </span>₹
-              {entry_fees_others}
-            </p>
-          </Glassmorphism>
-        </Col>
+        ></Col> */}
       </Row>
-    </>
+    </article>
   );
+
+  // const {
+  //   start_date,
+  //   end_date,
+  //   registration_end_date,
+  //   entry_fees_members,
+  //   entry_fees_others,
+  // } = details.info;
+
+  // return (
+  //   <>
+  //     <Glassmorphism className="mb-8 p-8 pt-2 md:m-5">
+  //       <h2
+  //         className={`text-5xl pt-3 md:text-7xl md:pt-0 font-bold italic text-center text-primary-dark`}
+  //       >
+  //         {details?.title}
+  //       </h2>
+  //     </Glassmorphism>
+
+  //     <Row justify="center" className="sm:px-8 md:px-16">
+  //       <Col
+  //         className="lg:pr-8"
+  //         span={24}
+  //         order={1}
+  //         sm={{ span: 22 }}
+  //         md={{ span: 20 }}
+  //         lg={{ span: 16, order: 0 }}
+  //       >
+  //         {details?.contents.map(
+  //           ({ type, image, description, text }, index) => (
+  //             <div key={`event_page_content_${index}`}>
+  //               {type === "image" && (
+  //                 <Glassmorphism className="relative overflow-hidden">
+  //                   <img className="opacity-80 w-full" src={image} />
+  //                   <div
+  //                     className={`absolute top-0 w-full h-full flex justify-center items-end bg-gradient-to-b from-transparent to-primary-dark  opacity-0 hover:opacity-100 transition-all duration-300`}
+  //                   >
+  //                     <p className="text-xl m-5 italic text-center text-white">
+  //                       {description}
+  //                     </p>
+  //                   </div>
+  //                 </Glassmorphism>
+  //               )}
+
+  //               {type === "text" && (
+  //                 <Glassmorphism className="my-5">
+  //                   <p className="text-xl m-5">{text}</p>
+  //                 </Glassmorphism>
+  //               )}
+  //             </div>
+  //           )
+  //         )}
+  //       </Col>
+  //       <Col
+  //         className="lg:pl-8 pb-5 lg:pb-0"
+  //         span={24}
+  //         order={0}
+  //         sm={{ span: 22 }}
+  //         md={{ span: 20 }}
+  //         lg={{ span: 8, order: 1 }}
+  //       >
+  //         <Glassmorphism className={`p-5 text-lg text-primary-dark`}>
+  //           <p className="my-2">
+  //             <span className={`font-semibold`}>Event Starts: </span>
+  //             <i>{start_date.toDateString()}</i>
+  //           </p>
+
+  //           <p className="my-2">
+  //             <span className="font-semibold">Event Ends: </span>
+  //             {end_date.toDateString()}
+  //           </p>
+
+  //           <p className="my-2">
+  //             <span className="font-semibold">Registration Ends: </span>
+  //             {registration_end_date.toDateString()}
+  //           </p>
+
+  //           <p className="my-2">
+  //             <span className="font-semibold">Entry Fees for Members: </span>
+  //             {entry_fees_members ? "₹" + entry_fees_members : "Free"}
+  //           </p>
+
+  //           <p className="my-2">
+  //             <span className="font-semibold">Entry Fees for Others: </span>₹
+  //             {entry_fees_others}
+  //           </p>
+  //         </Glassmorphism>
+  //       </Col>
+  //     </Row>
+  //   </>
+  // );
 };
 
 export default EventDetails;
