3ea8b4e97c36b67286328c2f7a93fc99a48227fb
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-10-01 00:47:32 +0530

    responsive dashboard

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,30 +1,44 @@
 "use client";
 import Glassmorphism from "@/components/common/glassmorphism";
 import { USER_DASHBOARD_SIDE_ITEMS } from "@/constants/dashboard";
-import { Layout, Menu } from "antd";
+import {
+  DoubleLeftOutlined,
+  DoubleRightOutlined,
+  IssuesCloseOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu, Grid } from "antd";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DashboardPageLayout = ({ children }) => {
-    const pathname = usePathname();
-    
+  const pathname = usePathname();
+  const { md } = Grid.useBreakpoint();
+  const [isSiderCollapsed, setSiderCollapsed] = useState(!md);
+
+  useEffect(() => {
+    setSiderCollapsed(!md);
+  }, [md]);
+
   const getSelectedMenuItemKey = () => {
     return `user_dashboard_side_item_${USER_DASHBOARD_SIDE_ITEMS.indexOf(
-        USER_DASHBOARD_SIDE_ITEMS.filter(({ href }) => href === pathname)[0]
+      USER_DASHBOARD_SIDE_ITEMS.filter(({ href }) => href === pathname)[0]
     )}`;
   };
 
   return (
     <>
-      <Layout className="!bg-transparent">
+      <Layout className="!bg-transparent relative">
         <Layout.Sider
-          className="!bg-transparent h-[calc(100vh-86px-40px)] mr-3"
-          width={256}
+          className="!bg-transparent h-[calc(100vh-86px-40px)] mr-0 md:mr-5"
+          width={md ? 256 : "100%"}
+          collapsedWidth={0}
+          collapsed={isSiderCollapsed}
         >
           <Glassmorphism className="h-full">
             <Menu
-              className="w-full !bg-transparent"
+              className="w-full !bg-transparent text-center md:text-start"
               selectedKeys={getSelectedMenuItemKey()}
             >
               {USER_DASHBOARD_SIDE_ITEMS.map(({ label, href }, index) => (
@@ -35,9 +49,28 @@ const DashboardPageLayout = ({ children }) => {
             </Menu>
           </Glassmorphism>
         </Layout.Sider>
-        <Layout.Content className="ml-2">
-          <Glassmorphism className="h-full">{children}</Glassmorphism>
-        </Layout.Content>
+        {(md || (!md && isSiderCollapsed)) && (
+          <Layout.Content>
+            <Glassmorphism className="h-full">{children}</Glassmorphism>
+          </Layout.Content>
+        )}
+        <div
+          className={`h-screen fixed top-0 ${
+            isSiderCollapsed ? "left-0" : "right-0"
+          } flex items-center md:hidden`}
+        >
+          <Button
+            className="!w-12 !h-12 !rounded-full !bg-transparent !shadow-2xl"
+            icon={
+              isSiderCollapsed ? (
+                <DoubleRightOutlined />
+              ) : (
+                <DoubleLeftOutlined />
+              )
+            }
+            onClick={() => setSiderCollapsed(!isSiderCollapsed)}
+          />
+        </div>
       </Layout>
     </>
   );
