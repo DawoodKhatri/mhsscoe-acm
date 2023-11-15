6e74bf441bef5ce8ea477bf6b7b45f6ae1c0c01c
Skyhero-alt <skyhero.code@gmail.com>
2023-11-15 17:24:16 +0530

    feat: Changed the landing page again

--- components/common/appBackground.jsx
+++ components/common/appBackground.jsx
@@ -25,11 +25,24 @@ const AppBackground = () => {
           );
         })}
       </div> */}
-      <img src="/images/bg-2.jpg" className="w-full h-full object-cover"/>
+      {/* <img src="/images/bg-2.jpg" className="w-full h-full object-cover"/> */}
       {/* <img
               className="object-cover align-middle absolute h-screen w-screen top-0"
               src="https://png.pngtree.com/background/20211215/original/pngtree-geometric-gradient-background-with-glassmorphism-effect-and-light-color-picture-image_1472036.jpg"
             /> */}
+
+      <div
+        class="pointer-events-none absolute inset-x-0 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-purple-400 to-primary-muted opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
--- components/home/heroSection.jsx
+++ components/home/heroSection.jsx
@@ -1,44 +1,69 @@
+"use client";
+
 import React from "react";
 import Glassmorphism from "../common/glassmorphism";
 import { Button } from "antd";
 import { colorPrimary } from "@/constants/colors";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const HeroSection = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    }); // Initialize AOS
+  }, []);
+
   return (
-    <Glassmorphism className="px-8 md:px-16">
-      <div className="w-full min-h-[calc(100vh-126px)]  justify-center  items-center flex flex-col ">
-        <div className="relative text-center w-full">
-          <h2
-            className="font-bold text-3xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-transparent"
-            style={{ WebkitTextStroke: `2px ${colorPrimary}` }}
-          >
-            MHSSCOE ACM
-          </h2>
-          <h2 className="font-bold text-3xl xs:text-6xl sm:text-7xl md:text-8xl lg:text-9xl w-full absolute top-0 left-[50%] -translate-x-[50%] text-primary animate-[water-flow-animation_4s_ease-in-out_infinite]">
-            MHSSCOE ACM
-          </h2>
-        </div>
-        <p className="text-primary-dark text-center mt-10 font-semibold uppercase text-sm">
-          MHSSCOE-ACM is a student chapter of ACM(Association for Computing
-          Machinery), the world’s largest educational and scientific computing
-          society. MHSSCOE-ACM was established in 2014 under the guidance of
-          teachers and with dedication of the students of IT department. Since
-          then, this committee aims to strengthen the students collective voice
-          through strong leadership, promotion of the highest standards, and
-          recognition of technical excellence. It is a team of students who
-          strive to organize and manage various technical and extra-curricular
-          events, workshops and competition.
-        </p>
-        <div className="flex gap-5 mt-7">
-          <Button className="mr-2 bg-primary" type="primary" size="large">
-            Meet the Team
-          </Button>
-          <Button className="ml-2 bg-primary" type="primary" size="large">
-            Login
-          </Button>
+    <div className="h-fit my-auto">
+      <div className="flex items-center justify-center overflow-auto">
+        <div className="relative isolate px-6 py-14 lg:px-8">
+          <div className="max-w-2xl">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Checkout our latest events!
+                <a
+                  target="_blank"
+                  className="font-semibold text-primary"
+                  href="#"
+                >
+                  <span className="absolute inset-0" aria-hidden="true"></span>
+                  More info<span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1
+                className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+                data-aos="fade-right"
+              >
+                ACM Chapter '23
+                <br />
+                <span className="text-5xl text-gray-500">M H Saboo Siddik</span>
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Empowering Minds, Igniting Futures and Building Leaders
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/80 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline focus-visible:outline-primary"
+                  href="/sign-in"
+                >
+                  Join Us
+                </a>
+                <a
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                  href="https://docs.uploadthing.com"
+                >
+                  Meet our team <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </Glassmorphism>
+    </div>
   );
 };
 
