ba036b692a6b6994018935cbb564980224f2900a
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-11-29 17:49:14 +0530

    Updated File Stream Logic

--- app/api/file/[folderPath]/[filePath]/route.js
+++ app/api/file/[folderPath]/[filePath]/route.js
@@ -1,6 +1,8 @@
-import { getFileStream } from "@/utils/firebaseStorage";
+import { getFileStream, getUrl } from "@/utils/firebaseStorage";
 import { NextResponse } from "next/server";
 
 export const GET = async (req, { params: { folderPath, filePath } }) => {
-  return new NextResponse(getFileStream(`${folderPath}/${filePath}`));
+  const fileUrl = await getUrl(`${folderPath}/${filePath}`);
+  const res = await fetch(fileUrl);
+  return new NextResponse(res.body);
 };
